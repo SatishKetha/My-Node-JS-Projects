61d3bb4b9f27b05aa01bd085650836f5299f9ac1
SatishKetha <venkatasatish004@gmail.com>
2023-01-10 08:01:19 +0000

    First commit in todo application node JS code

---
+++ app.js
@@ -0,0 +1,171 @@
+const express = require("express");
+const path = require("path");
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const app = express();
+
+app.use(express.json());
+
+const dbPath = path.join(__dirname, "todoApplication.db");
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
+
+const hasPriorityAndStatusProperties = (requestQuery) => {
+  return (
+    requestQuery.priority !== undefined && requestQuery.status !== undefined
+  );
+};
+
+app.get("/todos/", async (request, response) => {
+  const { search_q, priority, status } = request.query;
+
+  let data = null;
+  let getTodosQuery = "";
+
+  switch (true) {
+    case hasPriorityAndStatusProperties(request.query):
+      getTodosQuery = `
+      SELECT
+        *
+      FROM
+        todo 
+      WHERE
+        todo LIKE '%${search_q}%'
+        AND status = '${status}'
+        AND priority = '${priority}';`;
+      break;
+
+    case hasPriorityProperty(request.query):
+      getTodosQuery = `
+      SELECT
+        *
+      FROM
+        todo 
+      WHERE
+        todo LIKE '%${search_q}%'
+        
+        AND priority = '${priority}';`;
+      break;
+
+    case hasStatusProperty(request.query):
+      getTodosQuery = `
+      SELECT
+        *
+      FROM
+        todo 
+      WHERE
+        todo LIKE '%${search_q}%'
+        AND status = '${status}';`;
+      break;
+
+    default:
+      getTodosQuery = `
+      SELECT
+        *
+      FROM
+        todo 
+      WHERE
+        todo LIKE '%${search_q}%';`;
+  }
+
+  const data = await db.all(getTodosQuery);
+  response.send(data);
+});
+
+app.get("/todos/:todoId/", async (request, response) => {
+  let { todoId } = request.params;
+  let getTodoQuery = `
+    select * from todo where id = ${todoId};`;
+  let todo = await db.get(getTodoQuery);
+  response.send(todo);
+});
+
+app.post("/todos/", async (request, response) => {
+  const { id, todo, priority, status } = request.body;
+  const postTodoQuery = `
+  INSERT INTO
+    todo (id, todo, priority, status)
+  VALUES
+    (${id}, '${todo}', '${priority}', '${status}');`;
+  await db.run(postTodoQuery);
+  response.send("Todo Successfully Added");
+});
+
+app.put("/todos/:todoID", async (request, response) => {
+  let { todoID } = request.params;
+  let requestBody = request.body;
+  let columnToBeUpdated = "";
+
+  switch (true) {
+    case requestBody.status !== undefined:
+      columToBeUpdated = status;
+      break;
+
+    case requestBody.priority !== undefined:
+      columToBeUpdated = priority;
+      break;
+
+    case requestBody.todo !== undefined:
+      columToBeUpdated = todo;
+      break;
+  }
+
+  let previousTodoQuery = `
+    SELECT
+      *
+    FROM
+      todo
+    WHERE 
+      id = ${todoId};`;
+
+  const previousTodo = await database.get(previousTodoQuery);
+  //important  block. This is where the updated value is being assigned to the required column.
+  const {
+    todo = previousTodo.todo,
+    priority = previousTodo.priority,
+    status = previousTodo.status,
+  } = request.body;
+
+  const updateTodoQuery = `
+    UPDATE
+      todo
+    SET
+      todo='${todo}',
+      priority='${priority}',
+      status='${status}'
+    WHERE
+      id = ${todoId};`;
+
+  const updatedTodo = await db.run(updateTodoQuery);
+  response.send(`${columnToBeUpdated} updated`);
+});
+
+app.delete("/todos/:todoId/", async (request, response) => {
+  const { todoId } = request.params;
+  const deleteTodoQuery = `
+  DELETE FROM
+    todo
+  WHERE
+    id = ${todoId};`;
+
+  await db.run(deleteTodoQuery);
+  response.send("Todo Deleted");
+});
